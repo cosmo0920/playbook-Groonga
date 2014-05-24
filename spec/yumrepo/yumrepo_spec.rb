ab2e08810edc52deb322e228ea9722217af4bce3
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-05-25 01:18:26 +0900

    add addtional yumrepo spec

--- spec/yumrepo/yumrepo_spec.rb
+++ spec/yumrepo/yumrepo_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
+describe yumrepo('epel') do
+  it { should exist }
+  it { should be_enabled }
+end
+
 describe yumrepo('groonga') do
   it { should exist }
   it { should be_enabled }
