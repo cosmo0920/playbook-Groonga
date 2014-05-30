7d3f02e99e499752a0020ed27b14216cdfd0c40d
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-05-30 12:16:42 +0900

    specify `LANG=C` for fedora box

--- spec/fedorarepo/fedorarepo_spec.rb
+++ spec/fedorarepo/fedorarepo_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
 describe yumrepo('groonga') do
+  before :all do
+    RSpec.configure do |c|
+      c.disable_sudo = true
+    end
+  end
+
+  let(:pre_command) { 'export LANG=C' }
   it { should exist }
   it { should be_enabled }
+
+  before :all do
+    RSpec.configure do |c|
+      c.disable_sudo = false
+    end
+  end
 end
