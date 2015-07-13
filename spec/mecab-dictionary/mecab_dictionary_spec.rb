47540d2874dbfb9620b707ca47e0517712863d00
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2015-07-13 17:17:40 +0900

    Add mecab-ipadic task for Fedora

--- spec/mecab-dictionary/mecab_dictionary_spec.rb
+++ spec/mecab-dictionary/mecab_dictionary_spec.rb
@@ -4,6 +4,10 @@ describe package('mecab-ipadic'), :if => os[:family] == 'redhat' do
   it { should be_installed }
 end
 
+describe package('mecab-ipadic'), :if => os[:family] == 'fedora' do
+  it { should be_installed }
+end
+
 describe package('mecab-naist-jdic'), :if => os[:family] == 'ubuntu' do
   it { should be_installed }
 end
