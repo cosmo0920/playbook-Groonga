3886742a65f32a3c4a51c5010504782bab0e9998
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2015-07-13 16:16:14 +0900

    Remove serverspec 1.x heritage

--- spec/fedorarepo/fedorarepo_spec.rb
+++ spec/fedorarepo/fedorarepo_spec.rb
@@ -1,19 +1,6 @@
 require 'spec_helper'
 
 describe yumrepo('groonga') do
-  before :all do
-    RSpec.configure do |c|
-      c.disable_sudo = true
-    end
-  end
-
-  let(:pre_command) { 'export LANG=C' }
   it { should exist }
   it { should be_enabled }
-
-  after :all do
-    RSpec.configure do |c|
-      c.disable_sudo = false
-    end
-  end
 end
